24f43201404a46daacfff8404f7a28c1d55319ec
bb35319030 <bb35319030@ms.nagasaki.u.ac.jp>
2020-04-11 19:27:17 +0900

    提出します

--- k01/k01.c
+++ k01/k01.c
@@ -3,12 +3,19 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val,double ave,double n){
+    double aver = (n-1) * ave/n  + val/n ;
+    return aver;
+}
+extern double var_online(double val,double ave,double square_ave, double n){
+    double vari = (((n-1) *square_ave /n) + val * val/n) - (((n-1)*ave/n)+val/n)*(((n-1)*ave/n)+val/n);
+        return vari;
+
+}
 
 int main(void)
 {
-    double val;
+    double val,ave,square_ave=0,a,b,n=1,kogaku_ave,kogaku_var;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -27,17 +34,27 @@ int main(void)
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
 
-
-    
-
-
-
+        a=ave_online(val, ave,n);
+        b=var_online(val,ave,square_ave,n);
+        kogaku_ave=a;
+        kogaku_var=n*b/(n-1);
+        square_ave=ave_online(val*val,square_ave,n);
+        n=n+1;
+        ave=a;
+        
     }
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
+
+    printf("average: %f\n",a);
+    printf("variance: %f\n",b);
+    printf("kogaku_average: %f\n",kogaku_ave);
+    printf("kogaku_variance: %f\n",kogaku_var);
+
+
 
 
     return 0;
