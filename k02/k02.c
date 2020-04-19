97b2e2880fc65951c05290b0c0b5d3acaf721d2d
bb35319030 <bb35319030@ms.nagasaki.u.ac.jp>
2020-04-19 14:37:44 +0900

    k02提出します

--- k02/k02.c
+++ k02/k02.c
@@ -5,9 +5,10 @@
 
 extern double p_stdnorm(double z);
 
+
 int main(void)
 {
-    double val;
+    double val,Avea=170.8,Vara=5.43,Aveb=169.7,Varb=5.5,max_val,min_val,a,b;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -26,10 +27,13 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
-
+        
+        a=(val-Avea)/Vara;
+        max_val=p_stdnorm(a)*L1;
+        b=(val-Aveb)/Varb;
+        min_val=p_stdnorm(b)*L2;
+        L1=max_val;
+        L2=min_val;
 
 
     }
@@ -43,12 +47,10 @@ int main(void)
     printf("L_B: %f\n",min_val);
 
     return 0;
-
-
 }
-
 double p_stdnorm(double z)
 {
     return 1/sqrt(2*M_PI) * exp(-z*z/2);
 }
+
 
